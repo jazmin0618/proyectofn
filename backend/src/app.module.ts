31f2ab9578e7ebff82dfda3a648af63b54943dcf
jazmin0618 <jazmincarlossc@gmail.com>
2025-12-01 13:52:39 +0100

    module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,22 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller'; // Asume que existe
-import { AppService } from './app.service';     // Asume que existe
+import { AppController } from './app.controller'; 
+import {ConfigModule, ConfigService} from '@nestjs/config';
+import { AppService } from './app.service';     
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { config } from 'process';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
       type:'mysql',
-      host:'localhost',
-      port:3306,
-      username:'root',
-      password:'',
-      database:'baseia',
+      host:'dpg-d4motk7diees739dm8d0-a',
+      port:5432,
+      username:'backend_aah0_user',
+      password:'TTW18FzUCBSzxXzOfu9nX3XqhWM3mC4k',
+      database:'backend_aah0',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize:true,
       connectorPackage:'mysql2',
+      }),
     }),
     AuthModule,
     UsersModule,
