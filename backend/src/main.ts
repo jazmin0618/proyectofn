00ac67dc1c3d31c5cf02539ffe2ead17805e2ec3
jazmin0618 <jazmincarlossc@gmail.com>
2025-12-11 08:34:37 +0100

    main

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,10 +4,12 @@ import { AppModule } from './app.module' ;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000',
+    'https://proyectofn.vercel.app'],
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3001);
-  console.log(`Server running on port ${process.env.PORT ?? 3001}`);
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
+  console.log(`Server running on port ${port}`);
 }
 bootstrap();
---
+++ backend/src/recommendations/recommendations.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+import { RecommendationsService } from './recommendations.service';
+
+@Controller('recommendations')
+export class RecommendationsController {
+  constructor(private readonly recommendationsService: RecommendationsService) {}
+}
