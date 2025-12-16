4d28022b9c9b9fe317d3bfbac3c0d829d9c8e1a3
jazmin0618 <jazmincarlossc@gmail.com>
2025-12-16 17:55:49 +0100

    register

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,11 +3,12 @@ import { AppModule } from './app.module' ;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: ['http://localhost:3000',
-    'https://proyectofn.vercel.app'],
+    origin: 'https://proyectofn.vercel.app',
     credentials: true,
   });
+  
   const port = process.env.PORT || 3001;
   await app.listen(port);
   console.log(`Server running on port ${port}`);
